94438e4313c54a93e0bba6c484b3a1c33c6ea7a2
Derek <22509961+yuandere@users.noreply.github.com>
2025-02-19 16:16:53 -0800

    test: auth confirm

--- packages/frontend/src/routes/auth.confirm.tsx
+++ packages/frontend/src/routes/auth.confirm.tsx
@@ -1,41 +1,31 @@
-import { createFileRoute, Navigate, redirect } from "@tanstack/react-router";
+import { createFileRoute, Navigate } from "@tanstack/react-router";
 import { useAuth } from "../hooks/useAuth";
 
 export const Route = createFileRoute("/auth/confirm")({
-  beforeLoad: ({ context }) => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const tokenHash = urlParams.get("token_hash");
-    if (!tokenHash) {
-      throw redirect({ to: "/" });
-    }
-    context.auth.verifyOtp(tokenHash);
-  },
   component: RouteComponent,
 });
 
 function RouteComponent() {
   const auth = useAuth();
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const tokenHash = urlParams.get("token_hash");
-  // if (tokenHash) {
-  //   try {
-  //     auth.verifyOtp(tokenHash);
-  //   } catch (error) {
-  //     console.error("Error verifying OTP: ", error);
-  //     const err = error as Error;
-  //     const message = err.message || "Something went wrong";
-  //     return (
-  //       <div>
-  //         <p>Error verifying OTP: {message}</p>
-  //         <button onClick={() => window.location.reload()}>Try again</button>
-  //       </div>
-  //     );
-  //   }
-  // }
+  const urlParams = new URLSearchParams(window.location.search);
+  const tokenHash = urlParams.get("token_hash");
+  if (tokenHash) {
+    try {
+      auth.verifyOtp(tokenHash);
+    } catch (error) {
+      console.error("Error verifying OTP: ", error);
+      const err = error as Error;
+      const message = err.message || "Something went wrong";
+      return (
+        <div>
+          <p>Error verifying OTP: {message}</p>
+          <button onClick={() => window.location.reload()}>Try again</button>
+        </div>
+      );
+    }
+  }
   if (auth.isAuthenticated) {
     Navigate({ to: "/items" });
-  } else {
-    Navigate({ to: "/" });
   }
 
   return "Redirecting...";
