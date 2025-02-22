c211f8feef67760a044bd1f619766f081ee51604
Derek <22509961+yuandere@users.noreply.github.com>
2025-02-21 17:10:38 -0800

    feat: delete account

--- packages/backend/src/bindings.ts
+++ packages/backend/src/bindings.ts
@@ -3,6 +3,7 @@ import type { SupabaseClient, User } from "@supabase/supabase-js";
 export type Bindings = {
   SUPABASE_ANON_KEY: string;
   SUPABASE_URL: string;
+  SUPABASE_SERVICE_KEY: string;
 };
 
 export type Variables = {
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -6,6 +6,7 @@ import { zValidator } from "@hono/zod-validator";
 import { createClient, SupabaseClient, User } from "@supabase/supabase-js";
 import { z } from "zod";
 
+import { createSupabaseServerClient } from "./utils/supabase";
 import { authMiddleware } from "./middleware/auth";
 import { Bindings, Variables } from "./bindings";
 import items from "./routes/items";
@@ -44,6 +45,46 @@ app.get("/test", (c) => {
 // 	}
 // );
 
+app.delete("/api/v1/auth/delete", async (c) => {
+  const token = c.get("token");
+  const supabase = createClient(
+    c.env.SUPABASE_URL,
+    c.env.SUPABASE_SERVICE_KEY,
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+      },
+    }
+  );
+  try {
+    const {
+      data: { user },
+      error: userError,
+    } = await supabase.auth.getUser(token);
+    if (userError) throw userError;
+    if (!user) throw new Error("User not found");
+    const { error: deleteError } = await supabase.auth.admin.deleteUser(
+      user.id
+    );
+    if (deleteError) throw deleteError;
+
+    return c.json({
+      success: true,
+      message: "User account deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return c.json(
+      {
+        success: false,
+        error:
+          error instanceof Error ? error.message : "An unknown error occurred",
+      },
+      400
+    );
+  }
+});
 app.route("/api/v1/items", items);
 app.route("/api/v1/listItems", listItems);
 app.route("/api/v1/categories", categories);
